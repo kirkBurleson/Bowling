18201ff0bf65f4ee224962fe434feea0593463b9
Kirk Burleson <kirk.burleson@yahoo.com>
2015-05-06 10:24:40 -0500

    first try at logic finished.

--- bowling.c
+++ bowling.c
@@ -23,15 +23,44 @@ void pinsKnockedDown(int pins, struct BowlingState *state) {
 	int i;
 
 	state->frameThrow++;
-	state->gameThrow++;
-
-	// strike
-	if (state->frameThrow == 1 && pins == 10) {
-		state->scoreQueue[state->curFrame] = state->gameThrow + 2; // 2 more throws before we can score a strike
-		state->curFrame++;
-	}
-
 	
+	// max 21 throws in a game of bowling
+	if (++(state->gameThrow) > 21) 
+		return;
+
+	// record this throw
+	state->throws[state->gameThrow] = pins;
+
+	// first throw of frame
+	if (state->frameThrow == 1) {
+
+		//strike
+		if (pins == 10)
+			recordFrameScoringThrows(2);
+	}
+	// second throw of frame
+	else if (state->frameThrow == 2) {
+
+		// spare
+		if (state->throws[state->gameThrow] + state->throws[state->gameThrow - 1] == 10)
+			recordFrameScoringThrows(1);
+
+		// less than 10 pins knocked down this frame
+		else {
+			recordFrameScoringThrows(0);
+		}
+		
+		state->frameThrow = 0;
+	}	
+}
+
+void recordFrameScoringThrows(int futureThrows) {
+	// record the number of throws to add up to calculate a frame's score
+	state->scoreQueue[state->curFrame - 1] = state->gameThrow + futureThrows;
+
+	// There's only 10 frames in bowling
+	if (state->curFrame < 10)
+		state->curFrame++;
 }
 
 int getScoreForFrame(int frame, struct BowlingState *state) {
@@ -45,8 +74,7 @@ int getScoreForFrame(int frame, struct BowlingState *state) {
 	for (i = 0; i < state->scoreQueue[frame - 1]; i++)
 		score += state->throws[i];
 
-	return score
-;
+	return score;
 }
 
 struct BowlingState* createBowlingState() {
